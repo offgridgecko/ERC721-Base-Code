a5016a64936683e902860452e194e35b82c056ef
offgridgecko <68261911+offgridgecko@users.noreply.github.com>
2021-07-09 17:07:59 -0500

    Add files via upload

---
+++ interfaces.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ERC165 {
+    function supportsInterface(bytes4 interfaceID) external view returns (bool);
+}
+
+interface ERC721TokenReceiver {
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes calldata _data) external returns(bytes4);
+}
+
+interface ERC721Metadata {
+    function name() external view returns (string memory _name);
+    function symbol() external view returns (string memory _symbol);
+    function tokenURI(uint256 _tokenId) external view returns (string memory);
+}
